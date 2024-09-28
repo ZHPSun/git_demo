554ed54954be27514f10ab877bb0f908bb4ba2ed
Leo Sun <zhanpeng.sun899@gmail.com>
2024-09-29 08:08:17 +0930

    add contact us

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+
+app.get('/contact',(req,res)=>{
+    res.send('contact us')
+})
+
 app.listen(8080,()=>{
     console.log('Listening at port 8080')
 })

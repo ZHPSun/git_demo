ec7d336ef8d8ca48fe9a2231ebe2477144945619
Leo Sun <zhanpeng.sun899@gmail.com>
2024-09-29 08:02:28 +0930

    add about page

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+
+app.get('/about',(req,res)=>{
+    res.send('about us')
+})
+
 app.listen(8080,()=>{
     console.log('Listening at port 8080')
 })

643818c41a1f9be23ec389792a59ae7306ddf0e5
Leo Sun <zhanpeng.sun899@gmail.com>
2024-09-29 09:10:27 +0930

    add axios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-
+const axios=require('axios');
 const app=express();
 
 
@@ -7,13 +7,20 @@ app.get('/',(req,res)=>{
     res.send('hello world')
 })
 
-
 app.get('/contact',(req,res)=>{
     res.send('contact us')})
 
 app.get('/about',(req,res)=>{
     res.send('about us')
+})
 
+app.get('/posts',(req,res)=>{
+    
+    axios.get('http://localhost:3000/posts')
+    .then(function(resp){
+        res.json(resp.data)
+    })
+    .catch(function(error){console.error(error)})
 })
 
 app.listen(8080,()=>{
